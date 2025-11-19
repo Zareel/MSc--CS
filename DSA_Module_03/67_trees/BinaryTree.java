108a735758b238a6cf25b1b914a69c67ede59a88
Zreel <zareelbwd@gmail.com>
2025-11-19 05:51:18 +0530

    Binary Tree

---
+++ DSA_Module_03/67_trees/BinaryTree.java
@@ -0,0 +1,72 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+public class BinaryTree{
+    Node root;
+
+    public void insert(int data){
+        root = insertRec(root, data);
+    }
+
+    public Node insertRec(Node root ,int data){
+        if(root == null) 
+            root = new Node(data);
+        else if(data < root.data)
+            root.left = insertRec(root.left, data);
+        else if(data > root.data)
+            root.right = insertRec(root.right, data);
+        
+        return root;
+
+    }
+
+    // inorder
+    public void inorder(){
+        inorderRec(root);
+    }
+
+    public void inorderRec(Node root){
+        if(root != null){
+            inorderRec(root.left);
+            System.out.print(root.data + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    // preorder
+    public void preorder(){
+        preorderRec(root);
+    }
+
+    public void preorderRec(Node root){
+        if(root != null){
+              System.out.print(root.data + " ");
+        preorderRec(root.left);
+        preorderRec(root.right);
+
+        }
+
+    }
+
+       // postorder
+    public void postorder(){
+        postorderRec(root);
+    }
+
+    public void postorderRec(Node root){
+        if(root != null){
+            
+            postorderRec(root.left);
+            postorderRec(root.right);
+            System.out.print(root.data + " ");
+        }
+    };
+    
+}
